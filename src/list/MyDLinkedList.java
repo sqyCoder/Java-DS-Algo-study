53c5297f60b5ced2d4b01d68ce414b9dc843de34
Linsen-lsjs <1242654668@qq.com>
2026-08-11 20:07:16 +0800

    手搓双向链表

--- src/list/MyDLinkedList.java
+++ src/list/MyDLinkedList.java
@@ -21,10 +21,23 @@ public class MyDLinkedList {
             head = newNode;
             return;
         }
-
         newNode.next = head;
         head.prev = newNode;
         head = newNode;
+    }
+
+    public void addLast(int val) {
+        DLinkedNode newNode = new DLinkedNode(val);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        DLinkedNode cur = head;
+        while (cur.next != null) {
+            cur = cur.next;
+        }
+        cur.next = newNode;
+        newNode.prev = cur;
     }
 
     @Override
@@ -32,7 +45,6 @@ public class MyDLinkedList {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("[");
 
-        // 定义一个 tail, 当循环结束, tail 就指向最后一个节点.
         DLinkedNode tail = null;
         for (DLinkedNode cur = head; cur != null; cur = cur.next) {
             tail = cur;
@@ -43,7 +55,6 @@ public class MyDLinkedList {
         }
         stringBuilder.append("] | [");
 
-        // 添加一份反向打印的结果. 使用 | 来分割一下两份结果
         for (DLinkedNode cur = tail; cur != null; cur = cur.prev) {
             stringBuilder.append(cur.val);
             if (cur.prev != null) {
@@ -53,6 +64,14 @@ public class MyDLinkedList {
         stringBuilder.append("]");
 
         return stringBuilder.toString();
+    }
+
+    public int size() {
+        int size = 0;
+        for (DLinkedNode cur = head; cur != null; cur = cur.next) {
+            size++;
+        }
+        return size;
     }
 
     public void add(int index, int val) {
@@ -68,7 +87,6 @@ public class MyDLinkedList {
             addLast(val);
             return;
         }
-
         DLinkedNode cur = head;
         for (int i = 0; i < index; i++) {
             cur = cur.next;
@@ -76,15 +94,12 @@ public class MyDLinkedList {
         DLinkedNode prev = cur.prev;
         DLinkedNode newNode = new DLinkedNode(val);
 
-
         prev.next = newNode;
         newNode.prev = prev;
-
 
         newNode.next = cur;
         cur.prev = newNode;
     }
-
 
     public boolean contains(int value) {
         for (DLinkedNode cur = head; cur != null; cur = cur.next) {
@@ -103,8 +118,87 @@ public class MyDLinkedList {
             }
             index++;
         }
-
         return -1;
     }
 
+    public void removeFirst() {
+        if (head == null) {
+            return;
+        }
+        if (head.next == null) {
+            head = null;
+            return;
+        }
+        head = head.next;
+        head.prev = null;
+    }
+
+    public void removeLast() {
+        if (head == null) {
+            return;
+        }
+        if (head.next == null) {
+            head = null;
+            return;
+        }
+
+        DLinkedNode tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+        }
+        DLinkedNode prev = tail.prev;
+        prev.next = null;
+    }
+
+    public void remove(int index) {
+        int size = size();
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("下标越界: index=" + index);
+        }
+        if (index == 0) {
+            removeFirst();
+            return;
+        }
+        if (index == size - 1) {
+            removeLast();
+            return;
+        }
+        DLinkedNode prev = head;
+        for (int i = 0; i < index - 1; i++) {
+            prev = prev.next;
+        }
+        DLinkedNode toDelete = prev.next;
+        DLinkedNode next = toDelete.next;
+
+        prev.next = next;
+        next.prev = prev;
+    }
+
+    public void removeByValue(int value) {
+        if (head == null) {
+            return;
+        }
+
+        if (head.val == value) {
+            removeFirst();
+            return;
+        }
+        DLinkedNode cur = head;
+        for (; cur != null; cur = cur.next) {
+            if (cur.val == value) {
+                break;
+            }
+        }
+        if (cur == null) {
+            return;
+        }
+        DLinkedNode prev = cur.prev;
+        DLinkedNode next = cur.next;
+        if (prev != null) {
+            prev.next = next;
+        }
+        if (next != null) {
+            next.prev = prev;
+        }
+    }
 }
