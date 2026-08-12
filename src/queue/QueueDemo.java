bca789bb68f566c5235e36fe4957f43e5444155d
Linsen-lsjs <1242654668@qq.com>
2026-08-12 21:18:38 +0800

    测试队列

---
+++ src/queue/QueueDemo.java
@@ -0,0 +1,16 @@
+package queue;
+
+import java.util.*;
+
+public class QueueDemo {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        // 入队列
+        queue.offer(1);
+        // 出队列
+        Integer ret = queue.poll();
+        // 取队首元素
+        Integer peek = queue.peek();
+    }
